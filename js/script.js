995a898b529f86260175cbbe108fd7378faff307
Gustavo Feriani <guuh.feriani@gmail.com>
2024-05-10 20:53:20 -0300

    update changetheme

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const toggleTheme = document.getElementById("toggleTheme"); //
+
+const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function changeTheme () {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+
+
+
+
+
+
