c74db65a31e01440980891a2c330ef4442e7bf1d
Gustavo Feriani <guuh.feriani@gmail.com>
2024-05-14 20:01:52 -0300

    js final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-const toggleTheme = document.getElementById("toggleTheme"); //
-
+const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
 const accordionHeaders = document.querySelectorAll(".accordion-header");
 const menuLinks = document.querySelectorAll(".menu-link");
@@ -9,9 +8,26 @@ function changeTheme () {
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
-
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+// fim função mudar Tema do site
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+        
+    })
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
